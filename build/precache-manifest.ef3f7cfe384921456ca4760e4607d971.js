8bdd7050f6cc49e294ea9e1021cb1952a00710cf
Din Plo <DinPlo@test.com>
2020-10-23 13:41:06 +0200

    gitignore build

---
+++ build/precache-manifest.ef3f7cfe384921456ca4760e4607d971.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d95037fa5c6be73df78f917641b3d3",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "aeae0ef24b1215c9eba1",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2b3db84e36f9ff3181ab",
+    "url": "/FaceRecognition/static/css/main.269bd3d0.chunk.css"
+  },
+  {
+    "revision": "aeae0ef24b1215c9eba1",
+    "url": "/FaceRecognition/static/js/2.adfe0582.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.adfe0582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3db84e36f9ff3181ab",
+    "url": "/FaceRecognition/static/js/main.ec0a8113.chunk.js"
+  },
+  {
+    "revision": "a237c8afeb8aec7ea4a6",
+    "url": "/FaceRecognition/static/js/runtime-main.3fb5d59a.js"
+  },
+  {
+    "revision": "047807335840846c18a4b7f8213bd89b",
+    "url": "/FaceRecognition/static/media/brain.04780733.png"
+  }
+]);
